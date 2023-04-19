59ca29698005a7510f34db24de08b04af90f6542
sam <sam.woodcock@protonmail.com>
2023-04-19 08:26:06 +0100

    feat: python data processing example

---
+++ process_example/script.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+from pathlib import Path
+
+import pandas as pd
+
+
+logging.basicConfig(
+    level="DEBUG",
+    format=(
+        "%(asctime)s.%(msecs)03d [%(levelname)s] "
+        "%(name)s | %(funcName)s:%(lineno)d | %(message)s"
+    ),
+    datefmt="%y-%m-%d %H:%M:%S",
+    stream=sys.stdout,
+)
+
+log = logging.getLogger(__name__)
+
+
+url = "https://www.envidat.ch/dataset/d6c7a578-6317-49f3-8ba5-71f62b5b6610/resource/0aca99e1-7b3d-492f-a7bb-2756e5b74bbd/download/events.csv"
+
+log.info(f"Reading csv file into dataframe from url: {url}")
+df = pd.read_csv(url)
+
+log.info(f"Transposing dataframe {df}")
+df = df.transpose()
+
+filepath = Path(__file__).parent.resolve() / "output" / "processed.csv"
+log.info(f"Deleting output file if already exists: {filepath}")
+filepath.unlink(missing_ok=True)
+
+log.info("Writing output csv to file")
+df.to_csv(filepath)
